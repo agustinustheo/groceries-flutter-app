ab455ec1e2aaeea0bb9b793eafd8e488a37dd54b
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2021-03-21 19:43:06 +0700

    feat: use custom components start page .

--- lib/ui/intro/start_page.dart
+++ lib/ui/intro/start_page.dart
@@ -1,5 +1,7 @@
 import 'package:diantaraja_mobile/common/navigation.dart';
 import 'package:diantaraja_mobile/ui/dashboard/dashboard_page.dart';
+import 'package:diantaraja_mobile/widget/button/custom_button.dart';
+import 'package:diantaraja_mobile/widget/text/custom_text.dart';
 import 'package:flutter/material.dart';
 import 'package:diantaraja_mobile/common/sizes.dart';
 import 'package:diantaraja_mobile/common/string_image_asset.dart';
@@ -45,66 +47,49 @@ class _StartPageState extends State<StartPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             SizedBox(
-              height: Sizes.width(context) / 1.8,
-              width: Sizes.width(context) / 1.8,
+              height: Sizes.dp72(context),
+              width: Sizes.dp72(context),
               child: Image.asset(StringImageAsset.logo)
             ),
-            SizedBox(height: Sizes.dp25(context)),
-            SizedBox(height: Sizes.dp25(context)),
-            SizedBox(height: Sizes.dp25(context)),
+            SizedBox(height: Sizes.dp36(context)),
             SizedBox(
-              width: Sizes.width(context) / 1.8,
-              child: Text(
+              width: Sizes.dp72(context),
+              child: MontserratText(
                 'Saat ini layanan hanya tersedia untuk daerah rumah saya',
                 textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: Sizes.dp14(context),
-                  color: Colors.blue[400],
-                  fontFamily: 'Quicksand',
-                  fontWeight: FontWeight.bold
-                ),
+                fontSize: Sizes.dp14(context),
+                textColor: Colors.blue[400],
+                fontWeight: FontWeight.bold
               ),
             ),
             SizedBox(height: Sizes.dp18(context)),
             SizedBox(
-              width: Sizes.width(context) / 1.8,
-              child: Text(
+              width: Sizes.dp72(context),
+              child: MontserratText(
                 'Jarak kamu: 2,4 KM',
                 textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: Sizes.dp20(context),
-                  color: Colors.blue[400],
-                  fontFamily: 'Quicksand',
-                  fontWeight: FontWeight.w600
-                ),
+                fontSize: Sizes.dp20(context),
+                textColor: Colors.blue[400],
+                fontWeight: FontWeight.w600
               ),
             ),
             SizedBox(height: Sizes.dp25(context)),
-            SizedBox(
-              width: Sizes.width(context) / 1.8,
-              child: FlatButton(
-                color: Colors.blue[400],
-                onPressed: () {
-                  Navigation.intentWithoutBack(context, DashBoardPage());
-                },
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(0, 15.0, 0, 15.0),
-                  child: Text(
-                    "LANJUT",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: Sizes.dp20(context),
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 2,
-                    ),
-                  ),
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
+            FullFlatButton(
+              backgroundColor: Colors.blue[400],
+              margin: EdgeInsets.only(
+                left: Sizes.dp16(context),
+                top: Sizes.dp16(context),
+                right: Sizes.dp16(context)
               ),
+              child: MontserratText(
+                "LANJUT",
+                textColor: Colors.white,
+                fontSize: Sizes.dp20(context),
+                fontWeight: FontWeight.bold,
+              ),
+              onPressed: () {
+                Navigation.intentWithoutBack(context, DashBoardPage());
+              },
             ),
           ],
         ),
